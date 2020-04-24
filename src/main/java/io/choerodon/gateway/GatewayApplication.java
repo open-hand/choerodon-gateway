5159fac9f2e5866df667604eba22a821d2efb65a
lihao <310103270@qq.com>
2020-04-24 18:16:16 +0800

    [IMP] 添加sql连接配置

--- src/main/java/io/choerodon/gateway/GatewayApplication.java
+++ src/main/java/io/choerodon/gateway/GatewayApplication.java
@@ -3,12 +3,13 @@ package io.choerodon.gateway;
 
 import org.hzero.autoconfigure.gateway.EnableHZeroGateway;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableHZeroGateway
 @EnableDiscoveryClient
-
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
