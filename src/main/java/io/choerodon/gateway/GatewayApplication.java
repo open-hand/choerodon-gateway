140518d0d2065f82e9cb2d6a69af23f493c1fe09
lihao <310103270@qq.com>
2020-04-26 16:24:18 +0800

    [IMP] 去掉@SpringBootApplication

--- src/main/java/io/choerodon/gateway/GatewayApplication.java
+++ src/main/java/io/choerodon/gateway/GatewayApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableHZeroGateway
 @EnableDiscoveryClient
-@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
