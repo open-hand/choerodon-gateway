d63b9ac20b72ba618ff7c20b877f42569e38bb9d
scp <changping.shi@hand-china.com>
2020-05-14 22:43:35 +0800

    [ADD] 添加项目层过滤

---
+++ src/main/java/io/choerodon/gateway/infra/dto/ProjectUserDTO.java
@@ -0,0 +1,9 @@
+package io.choerodon.gateway.infra.dto;
+
+/**
+ * @author scp
+ * @date 2020/5/14
+ * @description
+ */
+public class ProjectUserDTO {
+}
---
+++ src/main/java/io/choerodon/gateway/infra/mapper/ProjectUserMapper.java
@@ -0,0 +1,12 @@
+package io.choerodon.gateway.infra.mapper;
+
+import io.choerodon.gateway.infra.dto.ProjectUserDTO;
+import io.choerodon.mybatis.common.BaseMapper;
+
+/**
+ * @author scp
+ * @date 2020/5/14
+ * @description
+ */
+public interface ProjectUserMapper extends BaseMapper<ProjectUserDTO> {
+}
