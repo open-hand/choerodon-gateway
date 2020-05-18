b8e7058ba997032d8c85c761714ba8a7271353e7
scp <changping.shi@hand-china.com>
2020-05-18 10:02:52 +0800

    [IMP] 修改项目层过滤

--- src/main/java/io/choerodon/gateway/filter/CustomPermissionCheckC7nFilter.java
+++ src/main/java/io/choerodon/gateway/filter/CustomPermissionCheckC7nFilter.java
@@ -2,18 +2,17 @@ package io.choerodon.gateway.filter;
 
 import java.util.List;
 
-import org.hzero.gateway.helper.entity.PermissionDO;
+import org.hzero.gateway.helper.config.GatewayHelperProperties;
+import org.hzero.gateway.helper.entity.CheckState;
 import org.hzero.gateway.helper.entity.RequestContext;
 import org.hzero.gateway.helper.service.CustomPermissionCheckService;
+import org.hzero.gateway.helper.util.UrlUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.util.CollectionUtils;
 
-import io.choerodon.core.exception.CommonException;
 import io.choerodon.gateway.infra.dto.ProjectUserDTO;
 import io.choerodon.gateway.infra.mapper.ProjectUserMapper;
-import io.choerodon.gateway.infra.util.SourceUtil;
 
 /**
  * @author scp
@@ -24,21 +23,28 @@ import io.choerodon.gateway.infra.util.SourceUtil;
 public class CustomPermissionCheckC7nFilter implements CustomPermissionCheckService {
     @Autowired
     private ProjectUserMapper projectUserMapper;
-    private static final String PROJECT_PATH_ID = "project_id";
-    private final AntPathMatcher matcher = new AntPathMatcher();
+    private static final String PROJECT_PERMISSION_CODE_FAILED = "error.permission.projectNotPass";
+    private static final String PROJECT_PERMISSION_NAME_FAILED = "PERMISSION_NOT_PASS_PROJECT";
+    private static final String PROJECT_PERMISSION_NAME_SUCCESS = "SUCCESS_PASS_PROJECT";
+    private static final String PROJECT_PERMISSION_CODE_SUCCESS = "success.permission.projectPass";
+
+    private GatewayHelperProperties properties;
 
     @Override
     public void checkPermission(RequestContext context) {
-        PermissionDO permission = context.getPermission();
-
-        Long projectId = SourceUtil.getSourceId(context.getTrueUri(), permission.getPath(), PROJECT_PATH_ID, matcher);
-
+        Long projectId = UrlUtils.parseLongValueFromUri(context.getTrueUri(), context.getPermission().getPath(), properties.getFilter().getCommonRequest().getParameterProjectId());
         ProjectUserDTO queryDTO = new ProjectUserDTO();
         queryDTO.setProjectId(projectId);
         queryDTO.setMemberId(context.getCustomUserDetails().getUserId());
         List<ProjectUserDTO> list = projectUserMapper.select(queryDTO);
+        CheckState checkState;
         if (CollectionUtils.isEmpty(list)) {
-            throw new CommonException("error.permission.projectNotPass");
+            checkState = CheckState.newState(405, PROJECT_PERMISSION_CODE_FAILED, PROJECT_PERMISSION_NAME_FAILED);
+        } else {
+            checkState = CheckState.newState(202, PROJECT_PERMISSION_CODE_SUCCESS, PROJECT_PERMISSION_NAME_SUCCESS);
+
         }
+        context.response.setStatus(checkState);
+
     }
 }
